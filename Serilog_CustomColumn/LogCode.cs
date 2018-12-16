3a1deede631f7e3905f3bb30aae9db634d87f6d7
Scott R. Sonnenwald <ssonnenwald@costsharingsolutions.com>
2018-12-16 15:15:53 -0500

    Initial Commit

---
+++ Serilog_CustomColumn/LogCode.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System;
+
+namespace ConsoleApp1
+{
+    public class LogCodeEnricher : ILogEventEnricher
+    {
+       public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {            
+            LogEventProperty logCode = propertyFactory.CreateProperty("logcode", "1");
+            logEvent.AddPropertyIfAbsent(logCode);
+        }
+    }
+}
---
+++ Serilog_CustomColumn/Program.cs
@@ -0,0 +1,56 @@
+﻿using NpgsqlTypes;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionstring = "User ID=Serilog;Password=Serilog;Host=localhost;Port=5432;Database=Logs";
+
+            string tableName = "logs";
+
+            //Used columns (Key is a column name) 
+            //Column type is writer's constructor parameter
+            IDictionary<string, ColumnWriterBase> columnWriters = new Dictionary<string, ColumnWriterBase>
+            {
+                {"message", new RenderedMessageColumnWriter() },
+                {"message_template", new MessageTemplateColumnWriter() },
+                {"logcode", new SinglePropertyColumnWriter("logcode", PropertyWriteMethod.Raw, NpgsqlDbType.Integer) },
+                {"level", new LevelColumnWriter() },
+                {"timestamp", new TimestampColumnWriter() },
+                {"exception", new ExceptionColumnWriter() },
+                {"log_event", new LogEventSerializedColumnWriter() }
+            };
+
+            var logger = new LoggerConfiguration()
+                                .MinimumLevel.Verbose()                                
+                                .Enrich.FromLogContext()
+                                //.Enrich.With<LogCodeEnricher>()
+                                .WriteTo.Console()
+                                .WriteTo.PostgreSQL(connectionstring,
+                                                    tableName,
+                                                    columnWriters,
+                                                    LogEventLevel.Verbose,
+                                                    null,
+                                                    null,
+                                                    30,
+                                                    null,
+                                                    false,
+                                                    "public",
+                                                    true)
+                                .CreateLogger();
+
+            logger.Information("{logcode} the message", 123);
+
+            // Flush and Dispose the logger.
+            ((IDisposable)logger).Dispose();            
+        }
+    }
+}
